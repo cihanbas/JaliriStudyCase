00b6a7cbf7a225237899595d5013f60414ff1c67
Cihan BAS <cihanbas07@gmail.com>
2022-01-18 14:13:37 +0300

    onpress added to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,9 +3,12 @@ import {Pressable, StyleSheet, Text} from 'react-native';
 import {appPadding, colors, normalize} from 'utils/constants';
 import {buttonTextStyle} from 'utils/typography';
 
-export function Button() {
+export function Button({disabled = false, onPress = () => {}}) {
   return (
-    <Pressable disabled style={styles.container}>
+    <Pressable
+      disabled={disabled}
+      onPress={onPress}
+      style={[styles.container, disabled && styles.disabled]}>
       <Text style={buttonTextStyle}>Buy Now</Text>
     </Pressable>
   );
@@ -13,9 +16,12 @@ export function Button() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.disableButton,
+    backgroundColor: '#007',
     padding: normalize(10),
     borderRadius: normalize(8),
     margin: appPadding,
+  },
+  disabled: {
+    backgroundColor: colors.disableButton,
   },
 });
